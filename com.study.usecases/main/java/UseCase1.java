351310318c655393063301cc7958c4ac9154c12b
anukulthalkar <anukul.thalkar@clairvoyantsoft.com>
2022-02-26 16:05:38 +0530

    MP:456- changes for conflict Scenario

---
+++ com.study.usecases/main/java/UseCase1.java
@@ -0,0 +1,61 @@
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.*;
+
+/*
+Get order count per customer for the month of 2014 January.
+ * Tables - orders and customers
+ * Data should be sorted in descending order by count and ascending order by customer id.
+ * Output should contain customer_id, customer_first_name, customer_last_name and customer_order_count.
+ */
+
+public class UseCase1 {
+
+    static final Logger logger = Logger.getLogger(UseCase1.class);
+    public final static String NEW_FILE = "new";
+
+    public static Dataset<Row> getOrders() {
+        String ordersPath = "C:\\Users\\Anukul Thalkar\\IdeaProjects\\UseCases\\src\\main\\resources\\retail_db\\orders\\part-00000";
+        Dataset<Row> orders = util.getSparkSession().read().format("csv").option("header", true).option("inferSchema", true).load(ordersPath);
+        return orders;
+    }
+
+    public static Dataset<Row> getCustomers() {
+        String customersPath = "C:\\Users\\Anukul Thalkar\\IdeaProjects\\UseCases\\src\\main\\resources\\retail_db\\customers\\part-00000";
+        Dataset<Row> customers = util.getSparkSession().read().format("csv").option("header", true).option("inferSchema", true).load(customersPath);
+        return customers;
+    }
+
+    public static Dataset<Row> getUseCase1Result(){
+        Dataset<Row> orders = getOrders();
+        Dataset<Row> customers = getCustomers();
+     Dataset<Row> result = orders.join(customers, orders.col("order_customer_id").equalTo(customers.col("customer_id"))).
+                where(orders.col("order_date").like("2014-01%")).
+                groupBy(customers.col("customer_id"),
+                        customers.col("customer_fname"),
+                        customers.col("customer_lname")).
+                agg(count(orders.col("order_customer_id")).alias("customer_order_count")).
+                orderBy(col("customer_order_count").desc(),customers.col("customer_id"));
+     return result;
+    }
+
+    public static void main(String[] args){
+
+        logger.info("------------------------------------------running UseCase 1------------------------------------------------------");
+
+        getOrders().show();
+        getOrders().printSchema();
+        getCustomers().show();
+        getCustomers().printSchema();
+        getUseCase1Result().show();
+        getUseCase1Result().printSchema();
+
+        logger.info("------------------------------------------Write Result---------------------------------------------------------------");
+
+        getUseCase1Result().coalesce(1).write().option("header",true).mode("overwrite").csv("C:\\Users\\Anukul Thalkar\\IdeaProjects\\UseCases\\src\\main\\resources\\outputs\\UseCase1");
+
+        logger.info("--------------------------------------------Completed----------------------------------------------------------------");
+
+    }
+}
